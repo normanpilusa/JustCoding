9c320a6b64d0a89942547bcbc8e60951e3e9bd2c
Norman Pilusa <normanpilusa@users.noreply.github.com>
2017-10-15 20:20:14 +0200

    Brute force program for finding arithmetic modulo

---
+++ roots.py
@@ -0,0 +1,29 @@
+#Program to find all the square roots of a number x under arithmetic modulo n. 
+#It finds all the integers a, 0 < a < n, such that a^2 ≡ x mod n.
+
+import sys
+
+def roots(x, n):
+	mod = x % n
+	roots = ""
+	
+	for number in range(n):
+		square = number*number
+		
+		if(square == mod):
+			roots += str(number) + " "
+			
+		elif(square > n):
+			if square % n == mod: roots += str(number) + " "
+			
+	return roots
+
+if __name__ == '__main__':
+	n = int(sys.argv[1])
+	x = int(sys.argv[2])
+	
+	print roots(x, n)
+	
+
+
+
